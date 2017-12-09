bbdc1a8ed378de788ebef2c0400e6d591f1ae5c7
Álan Crístoffer <acristoffers@gmail.com>
2017-12-09 16:13:03 -0200

    Adds port A0

--- ahio/drivers/arduino.py
+++ ahio/drivers/arduino.py
@@ -37,7 +37,7 @@ class Driver(ahio.abstract_driver.AbstractDriver):
 
     Pins = Enum(
         'Pins',
-        'D0 D1 D2 D3 D4 D5 D6 D7 D8 D9 D10 D11 D12 D13 A1 A2 A3 A4 A5')
+        'D0 D1 D2 D3 D4 D5 D6 D7 D8 D9 D10 D11 D12 D13 A0 A1 A2 A3 A4 A5')
 
     AnalogReferences = Enum('AnalogReferences', 'Default Internal External')
 
