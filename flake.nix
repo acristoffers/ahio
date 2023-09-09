5562f60bdd49687b696b794f1930f181a8c7f9b0
Álan Crístoffer <acristoffers@startmail.com>
2023-09-09 13:12:14 +0200

    chore: adds a nix flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "I/O Communication Library";
+  inputs = {
+    flake-utils.url = github:numtide/flake-utils;
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      rec {
+        ahio = pythonPkgs: pythonPkgs.buildPythonPackage rec {
+          format = "pyproject";
+          name = "ahio";
+          src = ./.;
+          propagatedBuildInputs = with pythonPkgs; [
+            poetry-core
+            pymodbus
+            pyserial
+            python-snap7
+          ];
+        };
+        packages.default = ahio pkgs.python311Packages;
+      });
+}
