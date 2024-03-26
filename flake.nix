cea713b1d511a9f0c42d6bf14c60294437b81035
Álan Crístoffer <acristoffers@startmail.com>
2024-03-26 09:13:37 -0300

    chore: lint flake

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
   description = "I/O Communication Library";
   inputs = {
-    flake-utils.url = github:numtide/flake-utils;
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
@@ -10,7 +10,7 @@
         pkgs = import nixpkgs { inherit system; };
       in
       rec {
-        ahio = pythonPkgs: pythonPkgs.buildPythonPackage rec {
+        ahio = pythonPkgs: pythonPkgs.buildPythonPackage {
           format = "pyproject";
           name = "ahio";
           src = ./.;
